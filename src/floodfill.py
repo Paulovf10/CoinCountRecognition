8eeb6ff3fa8d1a1b8b272008e5c6a7ea73bb0591
Joao Victor <jvcoutoo@gmail.com>
2023-06-26 19:45:58 -0300

    fix: floodfill method

--- src/floodfill.py
+++ src/floodfill.py
@@ -3,23 +3,17 @@ import numpy as np
 
 
 def floodfill_image(image):
-    # Use the Canny edge detector to find edges in the image
-    edges = cv2.Canny(image, 50, 150)
 
-    # Use a morphological closing operation to close gaps in the edges
-    kernel = np.ones((3, 3), np.uint8)
-    closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    # change background to white
+    im_floodfill_background = image.copy()
+    cv2.floodFill(im_floodfill_background, None, (0, 0), 255)
 
-    # Flood fill from point (0, 0), using the closed edges as the mask
-    im_floodfill = closing.copy()
-    h, w = closing.shape[:2]
-    mask = np.zeros((h+2, w+2), np.uint8)
-    cv2.floodFill(im_floodfill, mask, (20, 0), 255)
+    th, im_th = cv2.threshold(im_floodfill_background, 254, 255, cv2.THRESH_BINARY_INV);
 
     # Invert floodfilled image
-    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill_background)
 
     # Combine the two images to get the foreground
-    im_out = closing | im_floodfill_inv
+    im_out = im_th | im_floodfill_inv
 
     return im_out
